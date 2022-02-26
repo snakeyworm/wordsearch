56fcd548bb17c0c4b4a5d21154c219e7490542e1
snakeyworm <calebwloera@gmail.com>
2022-02-25 17:06:21 -0800

    Fixed breaking webpack change

--- presrc/App.js
+++ presrc/App.js
@@ -3,32 +3,80 @@
 
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
+import { NoEmitOnErrorsPlugin } from "webpack"
 import Board from "./Board"
 
 const styles = {
-    inputField: {
-        position: "absolute",
-        width: "33%",
-        height: "2%",
+    form: {
+        position: "relative",
         left: "50%",
-        transform: "translateX( -50% )",
-    }
+        top: 4,
+    },
+    inputField: {
+        padding: "0 30px",
+        width: window.screen.width * 0.1,
+        height: "3%",
+        // transform: "translateX( -50% )",
+        fontSize: "120%",
+        borderRadius:  "5px 5px",
+    },
+    submitButton: {
+        borderRadius: "0 10px 0 10px",
+        background: "#0000ee",
+        padding: "none",
+    },
 }
 
 // TODO Add aesthetics
-// App container
+// App crontainer
 function App() {
 
+    let [ inputBuf, setInputBuf ] = useState( "" )
     let [ input, setInput ] = useState( "" )
     let [ words, setWords ] = useState( [ "ice", "bible", "god", "computer", "hockey", "chocolate" ] )
 
+    // Handle user input
     let handleChange = ( event ) => {
-        setInput( event.target.value )
+        console.log( event )
+        setInputBuf( event.target.value )
+    }
+
+    // Handle user return
+    let handleKeyPress = ( event ) => {
+
+        if ( event.code === "Enter" ) {
+            submit()
+        }
+        
+    }
+
+    // Submit user input
+    let submit = () => {
+        setInput( inputBuf )
+        setInputBuf( "" )
+
     }
 
     return ( <div>
-        <input style={styles.inputField} type="text" value={input} onChange={handleChange}></input>
-        <Board words={words} answer={input} />
+        <div
+            style={styles.form}
+        >
+            <input 
+                style={styles.inputField}
+                type="text"
+                value={inputBuf}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}>
+            </input>
+            <input
+                style={styles.submitButton}
+                type="button"
+                value=" hello"
+                
+            >
+            </input>
+        </div>
+        {/* <Board words={words} answer={input} /> */}
     </div> )
 
 }
